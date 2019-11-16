4a65c75215dc7d4fc681efdb91b9861c04789651
Lucas Bertrand <lucas.bertrand@ensimag.grenoble-inp.fr>
2019-11-16 17:08:09 +0800

    Custom handler

--- x/requestchain/ante.go
+++ x/requestchain/ante.go
@@ -8,13 +8,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/auth/ante"
 )
 
-type ChainAnte struct{}
-
-func (ChainAnte) AnteHandle(ctx Context, tx Tx, simulate bool, next AnteHandler) (newCtx Context, err error {
-	fmt.Println(tx)
-}
-
-
 // Custom anteHandler to consider fee from block size
 func CustomAnteHandler(	ak auth.AccountKeeper,
 												fck auth.FeeCollectionKeeper,
@@ -26,4 +19,51 @@ func CustomAnteHandler(	ak auth.AccountKeeper,
 	antehandler := sdk.ChainAnteDecorators(cgtsd)
 
 	return antehandler;
+}
+
+func AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (sdk.Context, error) {
+	sigTx, ok := tx.(SigVerifiableTx)
+	if !ok {
+		return ctx, sdkerrors.Wrap(sdkerrors.ErrTxDecode, "invalid tx type")
+	}
+	params := cgts.ak.GetParams(ctx)
+	ctx.GasMeter().ConsumeGas(params.TxSizeCostPerByte*sdk.Gas(len(ctx.TxBytes())), "txSize")
+
+	// simulate gas cost for signatures in simulate mode
+	if simulate {
+		// in simulate mode, each element should be a nil signature
+		sigs := sigTx.GetSignatures()
+		for i, signer := range sigTx.GetSigners() {
+			// if signature is already filled in, no need to simulate gas cost
+			if sigs[i] != nil {
+				continue
+			}
+			acc := cgts.ak.GetAccount(ctx, signer)
+
+			var pubkey crypto.PubKey
+			// use placeholder simSecp256k1Pubkey if sig is nil
+			if acc == nil || acc.GetPubKey() == nil {
+				pubkey = simSecp256k1Pubkey
+			} else {
+				pubkey = acc.GetPubKey()
+			}
+			// use stdsignature to mock the size of a full signature
+			simSig := types.StdSignature{
+				Signature: simSecp256k1Sig[:],
+				PubKey:    pubkey,
+			}
+			sigBz := types.ModuleCdc.MustMarshalBinaryLengthPrefixed(simSig)
+			cost := sdk.Gas(len(sigBz) + 6)
+
+			// If the pubkey is a multi-signature pubkey, then we estimate for the maximum
+			// number of signers.
+			if _, ok := pubkey.(multisig.PubKeyMultisigThreshold); ok {
+				cost *= params.TxSigLimit
+			}
+
+			ctx.GasMeter().ConsumeGas(params.TxSizeCostPerByte*cost, "txSize")
+		}
+	}
+
+	return next(ctx, tx, simulate)
 }
